78e65da484b625c4f97d256ee34b09dc7dff2f25
eduardo-hdez <ehdez2004@gmail.com>
2026-04-25 12:10:45 -0600

    [Fix] cambiar modificar cantidad en carrito a AJAX en vez de POST

--- project/app.js
+++ project/app.js
@@ -26,6 +26,7 @@ app.use(helmet({
 }));
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
