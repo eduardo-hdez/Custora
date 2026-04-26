f7f1380919744969fedb51ba2b38edc6ad512578
AnaMachu <machucamiranda22@gmail.com>
2026-04-25 21:24:43 -0600

    [feat] navbar de cliente arreglada

---
+++ project/public/js/editar-prod.js
@@ -0,0 +1,11 @@
+ const modalError = document.getElementById('modalError');
+    const btnModalError = document.getElementById('btnModalError');
+    if (modalError && btnModalError) {
+      function ocultarModal() {
+        modalError.classList.add('hidden');
+      }
+      btnModalError.addEventListener('click', ocultarModal);
+      modalError.addEventListener('click', (e) => {
+        if (e.target === modalError) ocultarModal();
+      });
+    }
